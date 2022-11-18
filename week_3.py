2c3c77db9aa78b7cbeb135ad692e02d8548d65f9
Vanessa <vanessa.rodriguez.22@ucl.ac.uk>
2022-11-18 12:05:45 +0000

    Finished question 1 and implemented missclassifier

--- week_3.py
+++ week_3.py
@@ -14,7 +14,9 @@ A 4-panel figure, `week_3.pdf`, will be generated so you can check it's doing wh
 want. You should not need to edit the driver code, though you can if you wish.
 """
 
-import sys, os, os.path
+import sys
+import os
+import os.path
 import argparse
 import pprint
 
@@ -27,15 +29,15 @@ import pandas as pd
 import utils
 
 
-#### ADD YOUR CODE BELOW
+# ADD YOUR CODE BELOW
 
 # -- Question 1 --
-    
-def nearest_neighbours_predict ( train_X, train_y, test_X, neighbours=1 ):
+
+def nearest_neighbours_predict(train_X, train_y, test_X, neighbours=1):
     """
     Predict labels for test data based on neighbourhood in
     training set.
-    
+
     # Arguments:
         train_X: an array of sample data for training, where rows
             are samples and columns are features.
@@ -44,47 +46,61 @@ def nearest_neighbours_predict ( train_X, train_y, test_X, neighbours=1 ):
         test_X: an array of sample data to generate predictions for,
             in same layout as train_X.
         neighbours: how many neighbours to canvass at each test point
-        
+
     # Returns
         test_y: predicted labels for the samples in test_X
     """
-    assert(train_X.shape[0] == train_y.shape[0])
-    assert(train_X.shape[1] == test_X.shape[1])
+    assert (train_X.shape[0] == train_y.shape[0])
+    assert (train_X.shape[1] == test_X.shape[1])
 
-    # TODO: implement the k-nearest neighbours algorithm    
-    return None
-    
+    test_y = []
+    for x in test_X:
+        dist = []
+        for x_train in train_X:
+            dist.append(np.linalg.norm(x_train - x))
+        neighbors_ind = np.argsort(dist)[:neighbours]
+        labels = [train_y[i] for i in neighbors_ind]
+        test_y.append(utils.vote(labels))
+
+    return np.array(test_y)
+
 
 # -- Question 2 --
 
-def misclassification ( y, cls, weights=None ):
+def misclassification(y, cls, weights=None):
     """
     Calculate (optionally-weighted) misclassification error for
     a given set of labels if assigned the given class.
-    
+
     # Arguments
         y: a set of class labels
         cls: a candidate classification for the set
         weights: optional weights vector specifying relative
             importance of the samples labelled by y
-    
+
     # Returns
         err: the misclassification error of the candidate labels
     """
-    
-    # TODO: implement weighted misclassification metric
-    return 1
+
+    indicator = [1 if y[i] != cls else 0 for i in range(len(y))]
+
+    if weights is None:
+        weights = np.repeat(1/len(y), len(indicator))
+
+    err = np.dot(weights, indicator)
+
+    return err
 
 
-def decision_node_split ( X, y, cls=None, weights=None, min_size=3 ):
+def decision_node_split(X, y, cls=None, weights=None, min_size=3):
     """
     Find (by brute force) a split point that best improves the weighted
     misclassification error rate compared to the original one (or not, if
     there is no improvement possible).
-    
+
     Features are assumed to be numeric and the test condition is
     greater-or-equal.
-    
+
     # Arguments:
         X: an array of sample data, where rows are samples
             and columns are features.
@@ -96,7 +112,7 @@ def decision_node_split ( X, y, cls=None, weights=None, min_size=3 ):
         weights: optional weights vector specifying relevant importance
             of the samples
         min_size: don't create child nodes smaller than this
-    
+
     # Returns:
         feature: index of the feature to test (or None, if no split)
         thresh: value of the feature to test (or None, if no split)
@@ -105,18 +121,18 @@ def decision_node_split ( X, y, cls=None, weights=None, min_size=3 ):
         c1: class assigned to the set with feature >= thresh
             (or None, if no split)
     """
-    assert(X.shape[0] == len(y))
-    
+    assert (X.shape[0] == len(y))
+
     # TODO: implement this
     return None, None, None, None
 
 
-def decision_tree_train ( X, y, cls=None, weights=None,
-                          min_size=3, depth=0, max_depth=10 ):
+def decision_tree_train(X, y, cls=None, weights=None,
+                        min_size=3, depth=0, max_depth=10):
     """
     Recursively choose split points for a training dataset
     until no further improvement occurs.
-    
+
     # Arguments:
         X: an array of sample data, where rows are samples
             and columns are features.
@@ -130,7 +146,7 @@ def decision_tree_train ( X, y, cls=None, weights=None,
         min_size: don't create child nodes smaller than this
         depth: current recursion depth
         max_depth: maximum allowed recursion depth
-    
+
     # Returns:
         tree: a dict containing (some of) the following keys:
             'kind' : either 'leaf' or 'decision'
@@ -142,12 +158,12 @@ def decision_tree_train ( X, y, cls=None, weights=None,
     """
     # TODO: implement this
     return None
-    
 
-def decision_tree_predict ( tree, X ):
+
+def decision_tree_predict(tree, X):
     """
     Predict labels for test data using a fitted decision tree.
-    
+
     # Arguments
         tree: a decision tree dictionary returned by decision_tree_train
         X: an array of sample data, where rows are samples
@@ -162,10 +178,10 @@ def decision_tree_predict ( tree, X ):
 
 # -- Question 3 --
 
-def random_forest_train ( X, y, k, rng, min_size=3, max_depth=10 ):
+def random_forest_train(X, y, k, rng, min_size=3, max_depth=10):
     """
     Train a (simplified) random forest of decision trees.
-    
+
     # Arguments:
         X: an array of sample data, where rows are samples
             and columns are features.
@@ -175,20 +191,20 @@ def random_forest_train ( X, y, k, rng, min_size=3, max_depth=10 ):
         rng: an instance of numpy.random.Generator
             from which to draw random numbers        min_size: don't create child nodes smaller than this
         max_depth: maximum tree depth
-    
+
     # Returns:
         forest: a list of tree dicts as returned by decision_tree_train
     """
-    
+
     # TODO: implement this
     return None
-    
 
-def random_forest_predict ( forest, X ):
+
+def random_forest_predict(forest, X):
     """
     Predict labels for test data using a fitted random
     forest of decision trees.
-    
+
     # Arguments
         forest: a list of decision tree dicts
         X: an array of sample data, where rows are samples
@@ -203,11 +219,11 @@ def random_forest_predict ( forest, X ):
 
 # -- Question 4 --
 
-def adaboost_train ( X, y, k, min_size=1, max_depth=1, epsilon=1e-8 ):
+def adaboost_train(X, y, k, min_size=1, max_depth=1, epsilon=1e-8):
     """
     Iteratively train a set of decision tree classifiers
     using AdaBoost.
-    
+
     # Arguments:
         X: an array of sample data, where rows are samples
             and columns are features.
@@ -218,7 +234,7 @@ def adaboost_train ( X, y, k, min_size=1, max_depth=1, epsilon=1e-8 ):
         max_depth: maximum tree depth -- by default we just
             use decision stumps
         epsilon: threshold below which the error is considered 0
-    
+
     # Returns:
         trees: a list of tree dicts as returned by decision_tree_train
         alphas: a vector of weights indicating how much credence to
@@ -228,11 +244,11 @@ def adaboost_train ( X, y, k, min_size=1, max_depth=1, epsilon=1e-8 ):
     return None, None
 
 
-def adaboost_predict ( trees, alphas, X ):
+def adaboost_predict(trees, alphas, X):
     """
     Predict labels for test data using a fitted AdaBoost
     ensemble of decision trees.
-    
+
     # Arguments
         trees: a list of decision tree dicts
         alphas: a vector of weights for the trees
@@ -244,93 +260,114 @@ def adaboost_predict ( trees, alphas, X ):
     """
     # TODO: implement this
     return None
-    
 
-#### TEST DRIVER
+
+# TEST DRIVER
 
 def process_args():
-    ap = argparse.ArgumentParser(description='week 3 coursework script for COMP0088')
-    ap.add_argument('-s', '--seed', help='seed random number generator', type=int, default=None)
-    ap.add_argument('-n', '--num_samples', help='number of samples to use', type=int, default=50)
-    ap.add_argument('-k', '--neighbours', help='number of neighbours for k-NN fit', type=int, default=3)
-    ap.add_argument('-m', '--min_size', help='smallest acceptable tree node', type=int, default=3)
-    ap.add_argument('-w', '--weak', help='how many weak classifiers to train for AdaBoost', type=int, default=10)
-    ap.add_argument('-f', '--forest', help='how many trees to train for random forest', type=int, default=10)
-    ap.add_argument('-r', '--resolution', help='grid sampling resolution for classification plots', type=int, default=20)
-    ap.add_argument('-d', '--data', help='CSV file containing training data', default='week_3_data.csv')
-    ap.add_argument('file', help='name of output file to produce', nargs='?', default='week_3.pdf')
+    ap = argparse.ArgumentParser(
+        description='week 3 coursework script for COMP0088')
+    ap.add_argument(
+        '-s', '--seed', help='seed random number generator', type=int, default=None)
+    ap.add_argument('-n', '--num_samples',
+                    help='number of samples to use', type=int, default=50)
+    ap.add_argument('-k', '--neighbours',
+                    help='number of neighbours for k-NN fit', type=int, default=3)
+    ap.add_argument('-m', '--min_size',
+                    help='smallest acceptable tree node', type=int, default=3)
+    ap.add_argument(
+        '-w', '--weak', help='how many weak classifiers to train for AdaBoost', type=int, default=10)
+    ap.add_argument(
+        '-f', '--forest', help='how many trees to train for random forest', type=int, default=10)
+    ap.add_argument('-r', '--resolution',
+                    help='grid sampling resolution for classification plots', type=int, default=20)
+    ap.add_argument(
+        '-d', '--data', help='CSV file containing training data', default='week_3_data.csv')
+    ap.add_argument('file', help='name of output file to produce',
+                    nargs='?', default='week_3.pdf')
     return ap.parse_args()
+
 
 if __name__ == '__main__':
     args = process_args()
     rng = numpy.random.default_rng(args.seed)
-    
+
     print(f'loading data from {args.data}')
     df = pd.read_csv(args.data)
-    X = df[['X1','X2']].values[:args.num_samples,:]
+    X = df[['X1', 'X2']].values[:args.num_samples, :]
     y = df['Multi'].values[:args.num_samples]
 
     fig = plt.figure(figsize=(10, 10))
     axs = fig.subplots(nrows=2, ncols=2)
-    
+
     print(f'Q1: checking {args.neighbours}-nearest neighbours fit')
     # this is a fudge -- there's no training phase, so check implementation with a dummy prediction
-    dummy = nearest_neighbours_predict ( X[:2,:], y[:2], X[:2,:], neighbours=args.neighbours )
+    dummy = nearest_neighbours_predict(
+        X[:2, :], y[:2], X[:2, :], neighbours=args.neighbours)
     if dummy is None:
         print('decision tree not implemented')
-        utils.plot_unimplemented(axs[0,0], f'{args.neighbours}-Nearest Neighbours')
-    else:    
-        print(f'Q1: plotting {args.neighbours}-nearest neighbours fit')    
-        nn_cls = lambda z: nearest_neighbours_predict ( X, y, z, neighbours=args.neighbours )
-        utils.plot_classification_map(axs[0,0], nn_cls, X, y, resolution=args.resolution, title=f'{args.neighbours}-Nearest Neighbours')
-    
+        utils.plot_unimplemented(
+            axs[0, 0], f'{args.neighbours}-Nearest Neighbours')
+    else:
+        print(f'Q1: plotting {args.neighbours}-nearest neighbours fit')
+        def nn_cls(z): return nearest_neighbours_predict(
+            X, y, z, neighbours=args.neighbours)
+        utils.plot_classification_map(
+            axs[0, 0], nn_cls, X, y, resolution=args.resolution, title=f'{args.neighbours}-Nearest Neighbours')
+
     print('Q2: testing misclassification error')
     all_right = misclassification(np.ones(3), 1)
     all_wrong = misclassification(np.ones(3), 0)
-    fifty_fifty = misclassification(np.concatenate((np.ones(3), np.zeros(3))), 1)
-    
+    fifty_fifty = misclassification(
+        np.concatenate((np.ones(3), np.zeros(3))), 1)
+
     right_msg = 'correct' if np.isclose(all_right, 0) else 'wrong, should be 0'
     wrong_msg = 'correct' if np.isclose(all_wrong, 1) else 'wrong, should be 1'
-    fifty_msg = 'correct' if np.isclose(fifty_fifty, 0.5) else 'wrong should b 0.5'
-    
+    fifty_msg = 'correct' if np.isclose(
+        fifty_fifty, 0.5) else 'wrong should b 0.5'
+
     print(f' all right: {all_right} - {right_msg}')
     print(f' all wrong: {all_wrong} - {wrong_msg}')
     print(f' fifty-fifty: {fifty_fifty} - {fifty_msg}')
-    
+
     print('Q2: fitting decision tree')
-    tree = decision_tree_train ( X, y, min_size=args.min_size )
-    
+    tree = decision_tree_train(X, y, min_size=args.min_size)
+
     if tree is None:
         print('decision tree not implemented')
-        utils.plot_unimplemented(axs[0,1], 'Decision Tree')
+        utils.plot_unimplemented(axs[0, 1], 'Decision Tree')
     else:
         print('Q2: plotting decision tree fit')
-        tree_cls = lambda z: decision_tree_predict ( tree, z )
-        utils.plot_classification_map(axs[0,1], tree_cls, X, y, resolution=args.resolution, title='Decision Tree')
-    
+        def tree_cls(z): return decision_tree_predict(tree, z)
+        utils.plot_classification_map(
+            axs[0, 1], tree_cls, X, y, resolution=args.resolution, title='Decision Tree')
+
     print(f'Q3: fitting random forest with {args.forest} trees')
-    forest = random_forest_train ( X, y, args.forest, rng=rng, min_size=args.min_size )
-    
+    forest = random_forest_train(
+        X, y, args.forest, rng=rng, min_size=args.min_size)
+
     if forest is None:
         print('random forest not implemented')
-        utils.plot_unimplemented(axs[1,0], 'Random Forest')
+        utils.plot_unimplemented(axs[1, 0], 'Random Forest')
     else:
         print('Q3: plotting random forest fit')
-        forest_cls = lambda z: random_forest_predict ( forest, z )
-        utils.plot_classification_map(axs[1,0], forest_cls, X, y, resolution=args.resolution, title=f'Random Forest ({args.forest} Trees)')
-        
+        def forest_cls(z): return random_forest_predict(forest, z)
+        utils.plot_classification_map(
+            axs[1, 0], forest_cls, X, y, resolution=args.resolution, title=f'Random Forest ({args.forest} Trees)')
+
     print('Q4: fitting adaboost ensemble')
     # swap to binary labels since we're only doing 2-class AdaBoost
     y = df['Binary'].values[:args.num_samples]
-    trees, alphas = adaboost_train ( X, y, args.weak )
-    
+    trees, alphas = adaboost_train(X, y, args.weak)
+
     if trees is None:
         print('adaboost not implemented')
-        utils.plot_unimplemented(axs[1,1], 'AdaBoost')
-    else:   
+        utils.plot_unimplemented(axs[1, 1], 'AdaBoost')
+    else:
         print('Q4: plotting AdaBoost fit')
-        ada_cls = lambda z: adaboost_predict ( trees, alphas, z )
-        utils.plot_classification_map(axs[1,1], ada_cls, X, y, resolution=args.resolution, title=f'AdaBoost ({args.weak} Stumps)')
+        def ada_cls(z): return adaboost_predict(trees, alphas, z)
+        utils.plot_classification_map(
+            axs[1, 1], ada_cls, X, y, resolution=args.resolution, title=f'AdaBoost ({args.weak} Stumps)')
 
     fig.tight_layout(pad=1)
     fig.savefig(args.file)
